ab2b14d84fddd0e4f30d8561d4a1579d3dce1b02
isabela <isabelatargino89@gmail.com>
2024-12-07 09:59:35 -0300

    feat: inicialização da aplicação

---
+++ Gym-Plus/src/main/java/com/gymplus/Gym/Plus/GymPlusApplication.java
@@ -0,0 +1,13 @@
+package com.gymplus.Gym.Plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymPlusApplication.class, args);
+	}
+
+}
