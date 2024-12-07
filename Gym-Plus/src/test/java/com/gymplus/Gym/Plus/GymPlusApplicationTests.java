5477e0db7dc39d6fef9755cfbbecf020d0286d6a
isabela <isabelatargino89@gmail.com>
2024-12-07 10:13:26 -0300

    test: classe de testes

---
+++ Gym-Plus/src/test/java/com/gymplus/Gym/Plus/GymPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymplus.Gym.Plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
